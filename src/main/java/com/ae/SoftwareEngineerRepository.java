937f863c0ed14432addc02c0f136d17ddb52ac9a
Eschel Afik <afik.eschel@edu.fh-joanneum.at>
2025-08-04 16:06:15 +0200

    Initial commit: Spring Boot + Postgres demo

---
+++ src/main/java/com/ae/SoftwareEngineerRepository.java
@@ -0,0 +1,10 @@
+package com.ae;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoftwareEngineerRepository
+    extends JpaRepository<SoftwareEngineer, Integer> {
+
+}
+
+
